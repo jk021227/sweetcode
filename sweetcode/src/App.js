3a5de70da0f38249ba269091a93ea115993ccc44
Pauline Wee <paulinekowee@gmail.com>
2024-09-28 13:47:13 -0400

    pauline frontend

--- sweetcode/src/App.js
+++ sweetcode/src/App.js
@@ -1,35 +1,23 @@
 // import React from 'react';  // Add this line
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home' // your Home component
+import About from './pages/About' // your About component
+import Login from './pages/Login' // your Login component
+import NotFound from './pages/NotFound' // your NotFound component for 404
 
 function App() {
-  const [members, setMembers] = useState([]);
-
-  useEffect(() => {
-    // Fetch the members from the Flask backend
-    fetch('/members') // Ensure this URL points to your Flask backend
-      .then(response => response.json())
-      .then(data => setMembers(data.members))
-      .catch(error => console.error('Error fetching members:', error));
-  }, []);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>Hello Sweetcoders...Happy Hacking! <span>😊</span></code>
-        </p>
-      </header>
-      <h3>Here is the list of our members</h3>
-      <ul>
-        {members.map((member, index) => (
-          <li key={index}>{member}</li>
-        ))}
-      </ul>
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />{' '}
+        {/* Fallback for unmatched routes */}
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ sweetcode/src/pages/About.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react'
+
+function About() {
+  const [members, setMembers] = useState([])
+  useEffect(() => {
+    // Fetch the members from the Flask backend
+    fetch('/members') // Ensure this URL points to your Flask backend
+      .then((response) => response.json())
+      .then((data) => setMembers(data.members))
+      .catch((error) => console.error('Error fetching members:', error))
+  }, [])
+  return (
+    <div className="bg-slate-500">
+      <header>
+        <p className="">ABOUT PAGE</p>
+      </header>
+      <h3>Here is the list of our members</h3>
+      {/* <ul>
+        {members.map((member, index) => (
+          <li key={index}>{member}</li>
+        ))}
+      </ul> */}
+    </div>
+  )
+}
+
+export default About
